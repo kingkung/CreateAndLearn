e7bbd69df6743f679fcc6064197f449d1e8ecc6f
kingkung <kingkung82@gmail.com>
2020-11-22 17:50:13 -0800

    Memory Card Game - add inline comments - add SquareImageView so that width and height are always equal - minor optimizations

--- app/src/main/java/com/example/user/myapplication/memorygame/MemoryGameActivity.java
+++ app/src/main/java/com/example/user/myapplication/memorygame/MemoryGameActivity.java
@@ -18,11 +18,19 @@ import java.util.List;
 import java.util.Set;
 
 public class MemoryGameActivity extends AppCompatActivity {
+    // No card has been selected yet
+    private static final int NO_CARD_SELECTED = -1;
+    // The amount of time we show an incorrect match before showing the backs of the cards
+    private static final long DELAY_AFTER_INCORRECT_MATCH_IN_MILLIS = 2500L;
 
     private Handler handler = new Handler(Looper.getMainLooper());
+
+    // Views
     private GridLayout gridLayout;
     private View youWin;
     private View newGame;
+
+    // The array of cards - 2 of each
     private int[] cards = {
             R.drawable.bulbasaur,
             R.drawable.bulbasaur,
@@ -41,72 +49,104 @@ public class MemoryGameActivity extends AppCompatActivity {
             R.drawable.snorlax,
             R.drawable.snorlax,
     };
-    private boolean disableClicks;
+    // The deck of cards
     private List<Integer> deck;
+    // The set of cards which have been revealed (because we found their matches)
     private Set<Integer> revealed = new HashSet<>();
-
-    private int prevIndex = -1;
+    // Set this to true when a match is found
+    private boolean disableClicks;
+    // The index of the first selected card.
+    private int firstSelectedCard = NO_CARD_SELECTED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Flat for the "no title" feature, turning off the title at the top of the screen.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.memory_game_activity);
+        // Set components using findViewById
         gridLayout = findViewById(R.id.grid);
         youWin = findViewById(R.id.you_win);
         newGame = findViewById(R.id.new_game);
+        // Set the listener for the 'New Game' button
         newGame.setOnClickListener((view) -> startNewGame());
+        // Number of rows: 4, number of columns: 4, number of cards = 4 * 4 = 16
+        int numCards = gridLayout.getColumnCount() * gridLayout.getRowCount();
+        // Add a view to the gridLayout for each card on the screen.
+        for (int i = 0; i < numCards; i++) {
+            final int index = i;
+            // LayoutInflater - Converts a layout XML file into View objects
+            // inflate() - Inflate a new view hierarchy from the specified xml resource
+            ImageView view = (ImageView) getLayoutInflater().inflate(
+                    R.layout.memory_card, gridLayout, false);
+            // Set the listener for thc card
+            view.setOnClickListener((v) -> revealCard(view, index));
+            // Add the card to the grid layout
+            gridLayout.addView(view);
+        }
+        // Start the game.
         startNewGame();
     }
 
+    /**
+     * Starts a new game
+     * Resets deck list, revealed list, grid
+     * Shuffles deck
+     */
     private void startNewGame() {
+        // Turn off visibility for "You Win" text view
+        // Disable New Game button
         youWin.setVisibility(View.INVISIBLE);
         newGame.setEnabled(false);
-        int numCards = gridLayout.getColumnCount() * gridLayout.getRowCount();
+        // Add all of the cards into the deck
         deck = new ArrayList<>();
         for (int card : cards) {
             deck.add(card);
         }
+        // Shuffle the deck
         Collections.shuffle(deck);
+        // Clear the set of revealed cards.
         revealed.clear();
-        prevIndex = -1;
-        gridLayout.removeAllViews();
-        for (int i = 0; i < numCards; i++) {
-            final int index = i;
-            ImageView view = (ImageView) getLayoutInflater().inflate(
-                    R.layout.memory_card, gridLayout, false);
-            view.setOnClickListener((v) -> revealCard(view, index));
-            gridLayout.addView(view);
+        firstSelectedCard = NO_CARD_SELECTED;
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(R.drawable.card_back);
         }
     }
 
     private void revealCard(ImageView view, int index) {
-        if (disableClicks || prevIndex == index || revealed.contains(index)) {
+        // If a user clicks a card whose match is already found or is already currently turned over
+        if (disableClicks || firstSelectedCard == index || revealed.contains(index)) {
             return;
         }
         view.setImageResource(deck.get(index));
-        if (prevIndex == -1) {
-            prevIndex = index;
+        // If this is the first turned card, set prevIndex to index
+        if (firstSelectedCard == NO_CARD_SELECTED) {
+            firstSelectedCard = index;
             return;
         }
-        if (deck.get(prevIndex).equals(deck.get(index))) {
+        // If the two cards match
+        if (deck.get(firstSelectedCard).equals(deck.get(index))) {
+            // add both cards to the revealed list
             revealed.add(index);
-            revealed.add(prevIndex);
-            prevIndex = -1;
+            revealed.add(firstSelectedCard);
+            // reset prevIndex
+            firstSelectedCard = NO_CARD_SELECTED;
             if (revealed.size() == deck.size()) {
                 youWin.setVisibility(View.VISIBLE);
                 newGame.setEnabled(true);
             }
+            return;
         } else {
+            // Reveal the incorrect match for a set amount of time, then show the card backs again
             disableClicks = true;
             handler.postDelayed((Runnable) () -> {
-                ((ImageView) gridLayout.getChildAt(prevIndex))
+                ((ImageView) gridLayout.getChildAt(firstSelectedCard))
                         .setImageResource(R.drawable.card_back);
                 ((ImageView) gridLayout.getChildAt(index))
                         .setImageResource(R.drawable.card_back);
-                prevIndex = -1;
+                firstSelectedCard = NO_CARD_SELECTED;
                 disableClicks = false;
-            }, 2500);
+            }, DELAY_AFTER_INCORRECT_MATCH_IN_MILLIS);
         }
     }
 }
---
+++ app/src/main/java/com/example/user/myapplication/memorygame/SquareImageView.java
@@ -0,0 +1,24 @@
+package com.example.user.myapplication.memorygame;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+public class SquareImageView extends AppCompatImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+}
